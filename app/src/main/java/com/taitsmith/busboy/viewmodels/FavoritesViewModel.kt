1834c9755197fe3844433c98e8b7ed06e28b98d3
tait smith <tait@taitsmith.com>
2024-04-18 08:42:29 -0700

    remove unused code in favorites viewmodel

--- app/src/main/java/com/taitsmith/busboy/viewmodels/FavoritesViewModel.kt
+++ app/src/main/java/com/taitsmith/busboy/viewmodels/FavoritesViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.taitsmith.busboy.data.Stop
 import com.taitsmith.busboy.di.DatabaseRepository
+import com.taitsmith.busboy.di.StatusRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -12,24 +13,18 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class FavoritesViewModel @Inject constructor(application: Application,
-                                             private val databaseRepository: DatabaseRepository
-                                            ): AndroidViewModel(application) {
+class FavoritesViewModel @Inject constructor(
+    application: Application,
+    private val databaseRepository: DatabaseRepository,
+    private val statusRepository: StatusRepository
+): AndroidViewModel(application) {
 
     fun getFavoriteStops(): Flow<List<Stop>> = databaseRepository.getAllStops()
-
-    companion object {
-        lateinit var favoriteStops: MutableList<Stop>
-    }
 
     fun deleteStop(stop: Stop) {
         viewModelScope.launch(Dispatchers.IO) {
             databaseRepository.deleteStop(stop)
         }
-//        mutableStatusMessage.value = "STOP_DELETED"
-    }
-
-    init {
-        favoriteStops = ArrayList()
+        statusRepository.updateStatus("STOP_DELETED")
     }
 }
